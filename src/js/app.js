a88a93d04b21f2f13267631c3f16e89c49225ae7
Andrii <andrii@display.io>
2018-05-08 16:38:30 +0300

    Add previewurl field

--- src/js/app.js
+++ src/js/app.js
@@ -62,6 +62,12 @@ var DIOInt = (function (data) {
     show: function () {
       this.el.style.opacity = 1;
     },
+    applyPreviewUrl: function () {
+      helpers.DOMCollectionApply(this.el.querySelectorAll('.dio-intint__cta'), function (link) {
+        console.log(data.previewurl);
+        link.href = data.previewurl;
+      });
+    },
     create: function () {
       this.el = document.querySelector('.dio-intint__container');
       this.slider = slider;
@@ -70,6 +76,7 @@ var DIOInt = (function (data) {
       this.delegateEvents();
       this.adjustForOrientation();
       this.initBanner();
+      this.applyPreviewUrl();
       this.applyTitle();
       this.applyRating();
       this.slider.create();
@@ -229,12 +236,25 @@ var DIOInt = (function (data) {
   Ad.create();
 });
 
-if (MARKETPLACE || DEVELOPMENT) {
+if (MARKETPLACE) {
   DIOInt({
     images: [],
-    title: "",
-    rate: ""
+    title: '',
+    rate: '',
+    previewurl: ''
+  });
+} else if (DEVELOPMENT) {
+  DIOInt({
+    images: [
+      'http://wallpaperstock.net/banner-peak_wallpapers_27665_320x480.jpg',
+      'http://wallpaperstock.net/banner-peak_wallpapers_27665_320x480.jpg',
+      'http://wallpaperstock.net/banner-peak_wallpapers_27665_320x480.jpg'
+    ],
+    title: 'Hello world!',
+    rating: 3,
+    previewurl: 'http://example.com'
   });
 }
+
 
 module.exports = DIOInt;
